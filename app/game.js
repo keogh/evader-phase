1d9880b8348d4862c863774de943351526f69ba9
Isaac Zepeda <keogh24@gmail.com>
2015-05-07 16:57:28 -0700

    Add player to the game with initial position

--- app/game.js
+++ app/game.js
@@ -1,4 +1,6 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(350, 500, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+var player;
 
 function preload() {
   game.load.image('sky', 'assets/sky.png');
@@ -8,6 +10,9 @@ function preload() {
 }
 
 function create() {
+  player = game.add.sprite(0, 0, 'ship');
+  player.x = game.width/2 - player.width/2;
+  player.y = game.height - player.height - 5;
 }
 
 function update() {
